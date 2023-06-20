e0c358c57bae441db3ed4104df448e3bb3fb242f
Travis Biehn <tbiehn@gmail.com>
2023-06-20 00:49:23 -0400

    Added embeddings support.

--- gpt-parallel.go
+++ gpt-parallel.go
@@ -206,11 +206,11 @@ func (g *GPTParallel) chatCompletionWithBackoff(req openai.ChatCompletionRequest
 
 	// Get encoding
 	model := req.Model
-	if strings.HasPrefix(model, "gpt-4") {
+	/*if strings.HasPrefix(model, "gpt-4") {
 		model = openai.GPT4
 	} else if strings.HasPrefix(model, "gpt-3.5-turbo") {
 		model = openai.GPT3Dot5Turbo
-	}
+	}*/
 
 	/*if rand.Float64() < .5 {
 		return "", "", fmt.Errorf("Just a synthetic error.")
@@ -372,6 +372,233 @@ func (g *GPTParallel) chatCompletionWithExponentialBackoff(name string, req open
 
 	err := backoff.RetryNotify(operation, backoffContext, notify)
 	return result, finish, err
+}
+
+/*
+*
+* Embeddings.
+*
+ */
+type VectorRequestWithCallback struct {
+	Request    openai.EmbeddingRequest
+	Callback   func(result VectorRequestResult)
+	Identifier string
+}
+
+// RequestResult: A struct containing the original request, the response, the finish reason, and any errors that occurred during the request.
+type VectorRequestResult struct {
+	Request    openai.EmbeddingRequest `json:"request"`
+	Vector     []float32               `json:"vector"`
+	Identifier string                  `json:"identifier"`
+	Err        error                   `json:"error,omitempty"`
+}
+
+// RunEmbeddingsChan: A method that executes requests received from a channel in parallel with the given concurrency level, manages progress bars and retries, and sends results to a channel.
+func (g *GPTParallel) RunEmbeddingsChan(requestsChan <-chan VectorRequestWithCallback, concurrency int) <-chan VectorRequestResult {
+	resultsChan := make(chan VectorRequestResult)
+
+	if concurrency <= 0 {
+		concurrency = 1
+	}
+
+	// Calculate the total number of tokens in all requests
+	var totalTokens int64
+
+	var overallBar *mpb.Bar
+	if g.Progress != nil {
+		overallBar = g.Progress.AddBar(totalTokens,
+			mpb.BarPriority(-1),
+			mpb.PrependDecorators(
+				decor.Name("Overall: "),
+				decor.CountersNoUnit(" (%d/%d)"),
+			),
+			mpb.AppendDecorators(
+				decor.OnComplete(
+					decor.AverageETA(decor.ET_STYLE_GO, decor.WCSyncWidth), "completed",
+				),
+			),
+		)
+	}
+
+	wg := sync.WaitGroup{}
+
+	for i := 0; i < concurrency; i++ {
+		wg.Add(1)
+		barName := fmt.Sprintf("Query # %d", i)
+		go func() {
+			defer wg.Done()
+			for reqWithCallback := range requestsChan {
+				totalTokens += int64(1)
+				if overallBar != nil {
+					overallBar.SetTotal(totalTokens, false)
+				}
+
+				result, err := g.embeddingWithExponentialBackoff(barName, reqWithCallback.Request)
+				go reqWithCallback.Callback(VectorRequestResult{
+					Request:    reqWithCallback.Request,
+					Vector:     result,
+					Identifier: reqWithCallback.Identifier,
+					Err:        err,
+				})
+
+				resultsChan <- VectorRequestResult{
+					Request:    reqWithCallback.Request,
+					Vector:     result,
+					Identifier: reqWithCallback.Identifier,
+					Err:        err,
+				}
+				if overallBar != nil {
+					overallBar.IncrBy(1)
+				}
+			}
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		if overallBar != nil {
+			overallBar.Abort(true)
+		}
+		close(resultsChan)
+	}()
+
+	return resultsChan
+}
+
+func (g *GPTParallel) embeddingsWithBackoff(req openai.EmbeddingRequest, bar *mpb.Bar) ([]float32, error) {
+	startTime := time.Now()
+
+	resp, err := g.Client.CreateEmbeddings(g.ctx, req)
+
+	iterDuration := time.Since(startTime)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if bar != nil {
+
+		bar.EwmaIncrInt64(int64(1)-bar.Current(), iterDuration)
+		bar.Wait()
+	}
+
+	vectors := resp.Data[0].Embedding // []float32 with 1536 dimensions
+	return vectors, nil
+}
+
+func (g *GPTParallel) embeddingWithExponentialBackoff(name string, req openai.EmbeddingRequest) ([]float32, error) {
+	var result []float32
+
+	//backoff.Retry contract only permits returning an error.
+	operation := func() error {
+		var bar *mpb.Bar
+		if g.Progress != nil {
+			g.Logger.Debugf("Adding a bar.")
+			bar = g.Progress.AddBar(int64(1),
+				mpb.PrependDecorators(
+					decor.Name(name),
+					//decor.CountersNoUnit(" (%d/%d)"),
+					decor.AverageSpeed(0, "(%.1f EPS)"),
+				),
+				mpb.AppendDecorators(
+					decor.OnComplete(
+						decor.EwmaETA(decor.ET_STYLE_GO, 60, decor.WCSyncWidth), "done",
+					),
+				),
+				mpb.BarRemoveOnComplete(),
+			)
+		}
+
+		resultx, err := g.embeddingsWithBackoff(req, bar)
+		g.Logger.Debug("Got this response;", resultx, err)
+
+		if err != nil {
+			g.Logger.Debug("Have an error.", err != nil)
+			if bar != nil {
+				//TODO: Extract bar to parent, manage retry status out there.
+				//bar.SetRefill(int64(0))
+				bar.Abort(true)
+				bar.Wait()
+			}
+			return err
+		}
+		result = resultx
+		return nil
+	}
+
+	ctx, cancel := context.WithCancel(g.ctx)
+
+	backoffContext := backoff.WithContext(g.BackoffSettings, ctx)
+
+	notify := func(err error, duration time.Duration) {
+		barName := name + " Retry"
+
+		g.Logger.Debugf("Error: %T %+v", err, err)
+		var apiErr *openai.APIError
+		if errors.As(err, &apiErr) {
+			g.Logger.Debugf("Received an APIError: %d, %s, %s, %s", "nil", apiErr.Type, apiErr)
+			//apiErr.StatusCode is 0 when Streaming.
+			switch apiErr.Type {
+			case "invalid_request_error":
+				g.Logger.Error("Invalid Request Error")
+				cancel()
+				break
+			}
+			barName = barName + " " + apiErr.Type
+		}
+		var reqErr *openai.RequestError
+		if errors.As(err, &reqErr) {
+			g.Logger.Errorf("Received RequestError: %s, %d", reqErr.Err, "nil")
+		}
+
+		//This error doesn't follow the standard.
+		if strings.Contains(err.Error(), "You didn't provide an API key.") {
+			g.Logger.Error("No API Key was provided.")
+			cancel()
+		}
+
+		//We are backed off for time.Duration.
+		//Render a 'retrying bar' that completes with duration is up.
+		if g.Progress != nil {
+
+			retryBar := g.Progress.AddBar(int64(duration),
+				mpb.PrependDecorators(
+					decor.Name(barName),
+					//decor.CountersNoUnit(" (%d/%d)"),
+				),
+				mpb.AppendDecorators(
+					decor.OnComplete(
+						decor.AverageETA(decor.ET_STYLE_GO, decor.WCSyncWidth), "retrying",
+					),
+				),
+				mpb.BarRemoveOnComplete(),
+			)
+
+			ticker := time.NewTicker(10 * time.Millisecond)
+			startTime := time.Now()
+			for {
+				select {
+				case <-g.ctx.Done():
+					ticker.Stop()
+					retryBar.Abort(false)
+					retryBar.Wait()
+					return
+				case <-ticker.C:
+					elapsed := time.Since(startTime)
+					if elapsed >= duration {
+						ticker.Stop()
+						retryBar.SetCurrent(int64(duration))
+						return
+					}
+					retryBar.SetCurrent(int64(elapsed))
+				}
+			}
+		}
+
+	}
+
+	err := backoff.RetryNotify(operation, backoffContext, notify)
+	return result, err
 }
 
 // We've omitted 'Fatal' errors. This library shouldn't cause any panics or os.Exit()s.
